3960f3f781ef587219cd8eaafa8a0f67280b94f4
ahmed1c80 <ahmed1c80@gmail.com>
2025-03-14 22:44:13 +0300

    first commit2

--- app.py
+++ app.py
@@ -89,7 +89,8 @@ def upload_image():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
+    #app.run(debug=True)
 '''
 import pytesseract
 from PIL import Image
