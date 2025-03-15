83c02788c85da956d97a54ecdee1a316eadffd49
ahmed1c80 <ahmed1c80@gmail.com>
2025-03-16 00:36:52 +0300

    add ocr

--- app.py
+++ app.py
@@ -2,28 +2,38 @@ import pytesseract
 from PIL import Image
 import base64
 import io
+import requests
 from flask import Flask, request, render_template,jsonify
-
+#import easyocr
 import os
 app = Flask(__name__)
 
 
 
 
+# إنشاء قارئ EasyOCR (تحديد اللغة العربية)
+#reader = easyocr.Reader(['ar'])
+
+# قراءة النص من الصورة
+
+
+# عرض النتائج
+
+
 # جلب إصدار Tesseract
-tesseract_version = pytesseract#.get_tesseract_version()
-print(f"إصدار Tesseract المثبت: {tesseract_version}")
+#tesseract_version = pytesseract#.get_tesseract_version()
+#print(f"إصدار Tesseract المثبت: {tesseract_version}")
 
 # جلب مسار tesseract
 #tesseract_path = pytesseract.pytesseract.tesseract_cmd
 #print(f"مسار Tesseract: {tesseract_path}")
 #print(f'********{pytesseract.pytesseract.tesseract_cmd}')
 # تحديد المسار إلى ملفات اللغة
-pytesseract.pytesseract.tesseract_cmd = 'tesseract'  # إذا كان Tesseract مثبتًا
-os.environ['TESSDATA_PREFIX'] = os.path.join(os.getcwd(), 'tessdata')
+#pytesseract.pytesseract.tesseract_cmd = '/usr/share/tesseract'  # إذا كان Tesseract مثبتًا
+#os.environ['TESSDATA_PREFIX'] = os.path.join(os.getcwd(), 'tessdata')
 # جلب مسار ملفات اللغة
-tessdata_prefix = os.environ.get('TESSDATA_PREFIX')
-print(f"مسار ملفات اللغة (): {tessdata_prefix}")
+#tessdata_prefix = os.environ.get('TESSDATA_PREFIX')
+#print(f"مسار ملفات اللغة (): {tessdata_prefix}")
 @app.route('/')
 def index():
     return render_template('ready_image.html')
@@ -35,18 +45,46 @@ def analyze_image():
         return jsonify({'error': 'لم يتم تحميل أي صورة!'}), 400
 
     try:
-        # تحويل base64 إلى صورة
+      
+    # تحويل base64 إلى بايتات
         image_data = data['image'].split(',')[1]  # إزالة الجزء الأول من base64
-        image = Image.open(io.BytesIO(base64.b64decode(image_data)))
-
+        image_bytes = io.BytesIO(base64.b64decode(image_data))
+        # تحويل base64 إلى صورة
+        #image_data = data['image'].split(',')[1]  # إزالة الجزء الأول من base64
+        #image = Image.open(io.BytesIO(base64.b64decode(image_data)))
+        #result = reader.readtext(image)
         # استخراج النص باستخدام pytesseract
-        text = pytesseract.image_to_string(image, lang='ara')
+        text =ocr_space(image_bytes,'K87444439688957')# pytesseract.image_to_string(image)
         return jsonify({'text': text})
     except Exception as e:
         print(f"*****{e}")
         return jsonify({'error': str(e)}), 500
 
 
+
+
+def ocr_space(image_bytes, api_key,language='ara'):
+        url = "https://api.ocr.space/parse/image"
+        response = requests.post(
+              url,
+              files={"file": image_bytes},
+              data={"apikey": api_key, "language": language}
+              )
+        return response.json()
+ 
+ 
+'''
+  with open(image_path, 'rb') as image_file:
+                response = requests.post(
+                    url,
+                    files={image_path: image_file},
+                    data={"apikey": api_key}
+                )
+            return response.json()
+'''
+        #result = ocr_space('path_to_your_image.png', 'YOUR_OCR_SPACE_API_KEY')
+        #print(result)
+        #return result
 
 # دالة لسرد الملفات في مسار معين
 def list_files_in_directory(path):
