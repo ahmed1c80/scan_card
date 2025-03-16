48773a6115fd4977b0573da610bda57cdd55639e
ahmed1c80 <ahmed1c80@gmail.com>
2025-03-16 04:56:12 +0300

    to debug fals

--- app.py
+++ app.py
@@ -130,4 +130,4 @@ def dir():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
